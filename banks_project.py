18af6f026182a85469c3c7ac9b8fa0a4d3f55615
Hlumelo Kambi <hlumelokambi@gmail.com>
2025-07-26 01:27:44 +0200

    Added mods on extract

--- banks_project.py
+++ banks_project.py
@@ -9,6 +9,16 @@ from bs4 import BeautifulSoup
 
 print("Hello, World!")
 
+log_file = 'code_log.txt'
+target_file = 'transformed_data.csv'
+
+url = 'https://web.archive.org/web/20230908091635/https://en.wikipedia.org/wiki/List_of_largest_banks'
+db_name = 'Movies.db'
+table_name = 'Top_50'
+csv_path = 'top_50_films.csv'
+df = pd.DataFrame(columns=["Film","Year","Rotten Tomatoes' Top 100"])
+count = 0
+
 # Code for ETL operations on Country-GDP data
 
 
@@ -27,6 +37,24 @@ def extract(url, table_attribs):
     ''' This function aims to extract the required
     information from the website and save it to a data frame. The
     function returns the data frame for further processing. '''
+
+    html_page = requests.get(url).text
+    data = BeautifulSoup(html_page, 'html.parser')
+    tables = data.find_all('tbody')
+    rows = tables[0].find_all('tr')
+
+    for row in rows:
+        if count < 25 :
+            col = row.find_all('td')
+            if len(col)!=0:
+                data_dict = {"Film": str(col[1].contents[0]),
+                            "Year": int(col[2].contents[0]),
+                            "Rotten Tomatoes' Top 100":str(col[3].contents[0])}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
+                count+=1
+        else:
+            break
 
     return df
 
